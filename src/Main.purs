045052189e865fbe414a9b17b50c5bb8b46b4f5c
Bernhard Mallinger <b.mallinger@gmx.at>
2019-06-20 14:39:58 +0200

    Read text file

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,16 @@
 module Main where
 
-import Prelude (Unit, show)
+import Prelude (Unit, show, bind)
 import Effect
 import Effect.Console (log)
 
+import Effect.Aff (launchAff)
+import Effect.Class (liftEffect)
+
+import Node.Encoding (Encoding(UTF8))
+import Node.FS.Sync (readTextFile)
+
 main :: Effect Unit
 main = do
-  log (show 3)
+  fileContents <- readTextFile UTF8 "input.csv"
+  log fileContents
